dd82cdc35d515d49af33b89dc07a5f2236a1b940
Khang <56704842+Flair74@users.noreply.github.com>
2026-03-08 18:34:11 -0400

    completed task 88888888

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,17 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [revealed, setRevealed] = useState<boolean>(false);
+    return (
+        <div>
+            <Button
+                onClick={() => {
+                    setRevealed(!revealed);
+                }}
+            >
+                {revealed ? "Hide Answer" : "Reveal Answer"}
+            </Button>
+            {revealed && <div>Answer: 42</div>}
+        </div>
+    );
 }
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,37 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(4);
+    const [inProgress, setInProgress] = useState<boolean>(false);
+
+    return (
+        <div>
+            <Button
+                onClick={() => {
+                    setInProgress(true);
+                    setAttemptsLeft(attemptsLeft - 1);
+                }}
+                disabled={inProgress || attemptsLeft <= 0}
+            >
+                Start Quiz
+            </Button>
+            <Button
+                onClick={() => {
+                    setAttemptsLeft(attemptsLeft + 1);
+                }}
+                disabled={inProgress}
+            >
+                Mulligan
+            </Button>
+            <Button
+                onClick={() => {
+                    setInProgress(false);
+                }}
+                disabled={!inProgress}
+            >
+                Stop Quiz
+            </Button>
+            Attempts Left: {attemptsLeft}
+        </div>
+    );
 }
